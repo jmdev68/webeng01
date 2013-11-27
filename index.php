88bc6872ca6f7e1929ece3b348be60d331feaff8
jmdev68 <jmdev@online.ms>
2013-11-27 22:19:33 +0100

    added HA-Kapitel-06

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>PHP Info!</title>
+</head>
+<body>
+<header>
+	<h1>PHP Info!</h1>
+</header>
+<?php 
+phpinfo();
+?>
+
+</body>
+</html>
